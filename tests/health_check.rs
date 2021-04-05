4b13e106bfcc477f1dec14a11bd22e1f1f94a65b
Matthew J Perez <matt@mperez.io>
2021-04-04 22:46:56 -0500

    Added db script, broke lib into mods

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,17 @@
+use zero2prod::startup::run;
 use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    // Get the new random port
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+
+    // return application address
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -23,17 +36,56 @@ async fn health_check_works() {
     // Check that the response body was empty
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-
 }
 
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
-    // Get the new random port
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
+#[actix_rt::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
-    // return application address
-    format!("http://127.0.0.1:{}", port)
+    // Act
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[actix_rt::test]
+async fn subscribe_returns_400_when_data_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customizable error message on failure
+            "The API did not fail with 400 Bad Request when payload was {}.",
+            error_message
+        );
+    }
 }
